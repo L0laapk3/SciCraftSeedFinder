cac3c1513f41220cc4b56508453b9651fa10d2c9
Tom Schumm <fwiffo@gmail.com>
2018-06-18 23:15:59 -0700

    Tune mushroom finder

--- src/com/scicraft/seedfinder/finder/QuadHutMushroomFinder.java
+++ src/com/scicraft/seedfinder/finder/QuadHutMushroomFinder.java
@@ -2,7 +2,7 @@ import com.scicraft.seedfinder.*;
 import java.util.*;
 
 public class QuadHutMushroomFinder extends QuadHutFinder {
-	private static final float MUSHROOM_FRACTION = 0.02f;
+	private static final float MUSHROOM_FRACTION = 0.018f;
 	private static final Biome[] MUSHROOM_BIOMES = {
 		Biome.mushroomIsland,
 		Biome.mushroomIslandShore,
@@ -16,11 +16,13 @@ public class QuadHutMushroomFinder extends QuadHutFinder {
 		if (!checker.quadHutsWillSpawn(hut, chunkLocations, generator)) {
 			return false;
 		}
+
 		if (!checker.hasOceanSpawn(generator, radius, worldSpawn)) {
 			return false;
 		}
+
 		return checker.hasBiomes(
-				generator, radius*2*32*16, worldSpawn.getX(), worldSpawn.getZ(),
-				MUSHROOM_BIOMES, MUSHROOM_FRACTION);
+				generator, radius*32*16*2, worldSpawn.getX(), worldSpawn.getZ(),
+				MUSHROOM_BIOMES, checker.WATER_BIOMES, MUSHROOM_FRACTION);
 	}
 }
